402f1731c437005a4ac1ee1de858783ef3661737
Gonzalo Franchino <gonzalo.franchino@gmail.com>
2020-11-22 12:02:27 -0300

    testing file io

--- complete-developers-guide/cards/deck_test.go
+++ complete-developers-guide/cards/deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 
@@ -17,4 +20,20 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d)-1] != "Four of Clubs" {
 		t.Errorf("Expected first card of Four of Clubs, but got %v", d[len(d)-1])
 	}
+}
+
+func TestSaveToFileAndNewDeckFromFile(t *testing.T) {
+
+	os.Remove("_decktesting")
+
+	d := newDeck()
+	d.saveToFile("_deckTesting")
+
+	loadedDeck := newDeckFromFile("_deckTesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected deck length of 16, but got %v", len(loadedDeck))
+	}
+
+	os.Remove("_deckTesting")
 }
