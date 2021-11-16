4bd6644b7951a6a37653ce9784976241f0d6611b
Gonzalo Franchino <gonzalo.franchino@gmail.com>
2021-11-16 19:12:08 +0100

    refactor

--- go-with-tests/iteration/repeat_test.go
+++ go-with-tests/iteration/repeat_test.go
@@ -3,15 +3,37 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	got := Repeat("a")
-	expected := "aaaaa"
 
-	if got != expected {
-		t.Errorf("expected %q but got %q", expected, got)
+	assertCorrectRepetition := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("repeating 5 times character 'a'", func(t *testing.T) {
+		got := Repeat("a", 5)
+		want := "aaaaa"
+
+		assertCorrectRepetition(t, got, want)
+	})
+
+	t.Run("repeating 2 times character 'a'", func(t *testing.T) {
+		got := Repeat("a", 2)
+		want := "aa"
+
+		assertCorrectRepetition(t, got, want)
+	})
+
+	t.Run("repeating 0 times character 'a'", func(t *testing.T) {
+		got := Repeat("a", 0)
+		want := ""
+
+		assertCorrectRepetition(t, got, want)
+	})
 }
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 5)
 	}
 }
