5421ad5f82541ee652eb648db8c4d57396f31c37
Gonzalo Franchino <gonzalo.franchino@gmail.com>
2021-11-17 21:24:17 +0100

    refactor

--- go-with-tests/arrays_slices/sum_test.go
+++ go-with-tests/arrays_slices/sum_test.go
@@ -4,12 +4,22 @@ import "testing"
 
 func TestSum(t *testing.T) {
 
-	numbers := [5]int{1, 2, 3, 4, 5}
-	got := Sum(numbers)
-	expected := 15
+	t.Run("collection of 5 numbers", func(t *testing.T) {
 
-	if got != expected {
-		t.Errorf("got %d but expected was %d given %v", got, expected, numbers)
-	}
+		numbers := []int{1, 2, 3, 4, 5}
+		got := Sum(numbers)
+		want := 15
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+		got := Sum(numbers)
+		want := 6
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
 
 }
