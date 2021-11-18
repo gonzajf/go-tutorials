e7883a7178821397956cec8552ef69f3cc9a31a1
Gonzalo Franchino <gonzalo.franchino@gmail.com>
2021-11-18 21:33:52 +0100

    sum of slices

--- go-with-tests/arrays_slices/sum.go
+++ go-with-tests/arrays_slices/sum.go
@@ -8,3 +8,15 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(numbersToSum... []int) []int {
+
+	lengthOfNumbers := len(numbersToSum)
+
+	sums := make([]int, lengthOfNumbers)
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+	return sums
+}
--- go-with-tests/arrays_slices/sum_test.go
+++ go-with-tests/arrays_slices/sum_test.go
@@ -1,6 +1,9 @@
 package arrays_slices
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
@@ -21,5 +24,14 @@ func TestSum(t *testing.T) {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
 		}
 	})
+}
 
+func TestSumAll(t *testing.T) {
+
+	got := SumAll([]int{1,2}, []int{0,9})
+	want := []int {3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v but wanted %v", got, want)
+	}
 }
